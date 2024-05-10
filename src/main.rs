9b443806fcb1c14dc081eec93275f880bd530126
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-05-10 23:44:10 +0200

    tokio add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<()> {
+    // setup terminal
+    // startup()?;
+    //
+    // let result = run();
+    //
+    // // teardown terminal before unwrapping Result of app run
+    // shutdown()?;
+    //
+    // result?;
+
+    Ok(())
 }
