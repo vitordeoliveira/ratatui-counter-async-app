30e86cd080dca83511a676b7bc37f56bd13c5b02
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-05-11 15:32:46 +0200

    event async handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use color_eyre::eyre::Result;
+use crossterm::event::KeyEvent;
+use futures::{FutureExt, StreamExt};
+use tokio::{sync::mpsc, task::JoinHandle};
+
+#[derive(Clone, Copy, Debug)]
+pub enum Event {
+    Error,
+    Tick,
+    Key(KeyEvent),
+}
+
+#[derive(Debug)]
+pub struct EventHandler {
+    _tx: mpsc::UnboundedSender<Event>,
+    rx: mpsc::UnboundedReceiver<Event>,
+    task: Option<JoinHandle<()>>,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     // setup terminal
